d3d4fd1b3802893d461dbf05fda5dbca26482bd4
davidcol <mezad55@gmail.com>
2023-10-18 22:55:14 -0500

    funciones

---
+++ js/16.js
@@ -0,0 +1,30 @@
+//funciones
+
+//declaración de una funcion
+
+function sumar () {
+
+    console.log(10 + 10);
+
+}
+
+
+//se manda a llamar o a ejecutar la funcion
+sumar();
+
+//Expresión de una funcion
+
+const sumar2 = function() {
+    console.log(3 + 3);
+}
+
+sumar2();
+
+
+//IIFE
+
+(function() {
+    console.log('esto es una funcion');
+})(); //esta tipo de funcion se manda a llamar ella misma se invoca ella misma no se recomienda para reutilizar
+
+
