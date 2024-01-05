302c75cc004784f9429c8c3f7150f16e4db97c82
davidcol <mezad55@gmail.com>
2024-01-04 21:59:21 -0500

    js.32

--- js/31.js
+++ js/31.js
@@ -1,5 +1,10 @@
 const boton = document.querySelector("#boton");
-boton.addEventListener('click', function() {
+boton.addEventListener('click', () => {
     Notification.requestPermission()
-        .then(resultado => console.log(`EL resultado es $(resultado)`))
+        .then(resultado => console.log(`EL resultado es ${resultado}`))
 });
+
+
+if(Notification.permission == 'granted') {
+    new Notification('esta es una notificacion,')
+}
