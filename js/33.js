0d383da179a1dae5097cf2927ed9ce573fb82b8e
davidcol <mezad55@gmail.com>
2024-01-08 16:09:49 -0500

    js.34

--- js/33.js
+++ js/33.js
@@ -1,12 +1,16 @@
-function obtenerEmpleados() {
-    const archivo = './js/empleados.json';
+async function obtenerEmpleados() {
+    const archivo = '/js/empleados.json';
 
-    fetch(archivo)
+    /* fetch(archivo)
         .then(resultado => resultado.json())
         .then(datos => {
             const {empleados} = datos;
             console.log(empleados);
-        })
+        }); */
+
+    const resultado = await fetch(archivo);
+    const datos = await resultado.json();
+    console.log(datos);
 }
 
 obtenerEmpleados();
